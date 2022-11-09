d918451a7f79d1268ab0bf331b8ec690ab8b6081
HolHermann <holhermann78@gmail.com>
2022-11-09 13:03:04 +0100

    MAJ

--- app2.js
+++ app2.js
@@ -10,21 +10,16 @@ allQuestions.forEach((element) => {
       gsap.to(currentChoice, {
         duration: 0.2,
         height: height + 40,
-        // opacity: 1,
         padding: "20px 15px",
         visibilily: "visible",
         visibility: "visible",
-        // background: "transparent",
       });
     } else if (this.childNodes[3].src.includes("up")) {
       this.childNodes[3].src = "/images/logoSite/chevron-down-solid.svg";
       gsap.to(currentChoice, {
         duration: 0.2,
         height: 0,
-        // opacity: 0,
         padding: "0px 15px",
-        // margin: "auto",
-        // background: "red",
         visibility: "hidden",
       });
     }
