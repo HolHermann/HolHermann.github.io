85e41de4cb0a11b36e7ea4d5f46e3745c4060a85
HolHermann <holhermann78@gmail.com>
2022-11-09 11:31:12 +0100

    MAJ

---
+++ app2.js
@@ -0,0 +1,32 @@
+const allQuestions = document.querySelectorAll(".visible-pannel");
+
+allQuestions.forEach((element) => {
+  element.addEventListener("click", function () {
+    const height = this.parentNode.childNodes[3].scrollHeight;
+    const currentChoice = this.parentNode.childNodes[3];
+
+    if (this.childNodes[3].src.includes("down")) {
+      this.childNodes[3].src = "/images/logoSite/chevron-up-solid.svg";
+      gsap.to(currentChoice, {
+        duration: 0.2,
+        height: height + 40,
+        // opacity: 1,
+        padding: "20px 15px",
+        visibilily: "visible",
+        visibility: "visible",
+        // background: "transparent",
+      });
+    } else if (this.childNodes[3].src.includes("up")) {
+      this.childNodes[3].src = "/images/logoSite/chevron-down-solid.svg";
+      gsap.to(currentChoice, {
+        duration: 0.2,
+        height: 0,
+        // opacity: 0,
+        padding: "0px 15px",
+        // margin: "auto",
+        // background: "red",
+        visibility: "hidden",
+      });
+    }
+  });
+});
